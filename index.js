e027336b42870003cd7e99728848c868ace005d4
S.Kuntsevich <umdomby@yandex.ru>
2022-01-24 18:26:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const WebSocketServer = require('ws').Server;
+
+
+const app = express();
+
+// Certificate
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/cyberbet.online/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/cyberbet.online/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/cyberbet.online/chain.pem', 'utf8');
+
+const path = require('path')
+
+const privateKey = fs.readFileSync(path.resolve(__dirname,'./cert/privkey.pem'));
+const certificate = fs.readFileSync(path.resolve(__dirname,'./cert/cert.pem'));
+const ca = fs.readFileSync(path.resolve(__dirname,'./cert/chain.pem'));
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+app.use((req, res) => {
+    res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+
+const wss = new WebSocketServer({
+    server: httpsServer
+});
+
+const dgram = require('dgram');
+
+const WebSocketSender = require("ws");
+const wsESP = new WebSocketSender('ws://192.168.0.107:81');
+
+wss.on('connection', (wsConnect) => {
+    wsESP.on('open', function open() {
+        wss.send(JSON.stringify({
+            id: '1',
+            username: 'username',
+            method: "connection",
+        }));
+    });
+    console.log('The server is started, listening~');
+    wsConnect.on('message', (message) => {
+        wsESP.send(message);
+        const PORT_UDP = 1234;
+        const HOST = '93.125.10.70';
+        const messageUDP = new Buffer('My KungFu is Good!');
+        const client = dgram.createSocket('udp4');
+        client.send(messageUDP, 0, messageUDP.length, PORT_UDP, HOST, function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP message sent to ' + HOST +':'+ PORT_UDP);
+            client.close();
+        });
+
+        console.log(`Server received: ${message}`);
+        wsConnect.send(`Server reply: ${message}`, (err) => {
+            if (err) {
+                console.log(`Server error: ${err}`);
+            }
+        });
+    });
+});
+
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
+
